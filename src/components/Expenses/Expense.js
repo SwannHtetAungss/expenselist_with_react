65dd00221c47f8eda60ca002ba6d81cfe98998f8
SwannHtetAung <swannhtetaung.ss@gmail.com>
2021-09-18 16:07:47 +0630

    show dynamic list

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -5,20 +5,29 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expense.css";
 
 const Expense = (props) => {
-    const [filterYear,setfilterYear] = useState("2020");
-    const filterChangeHandler = (selectedYear) => {
-        setfilterYear(selectedYear);
-    };
-    return (
-        <Card className="expenses">
-            <ExpensesFilter selected={filterYear} onChangeFilter={filterChangeHandler} />
+  const [filterYear, setfilterYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setfilterYear(selectedYear);
+  };
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filterYear}
+        onChangeFilter={filterChangeHandler}
+      />
 
-            <ExpanseItem title={props.expenses[0].title} date={props.expenses[0].date} amount={props.expenses[0].amount} />
-            <ExpanseItem title={props.expenses[1].title} date={props.expenses[1].date} amount={props.expenses[1].amount} />
-            <ExpanseItem title={props.expenses[2].title} date={props.expenses[2].date} amount={props.expenses[2].amount} />
-            <ExpanseItem title={props.expenses[3].title} date={props.expenses[3].date} amount={props.expenses[3].amount} />
-        </Card>
-    );
-}
+      {props.expenses.map((expense) => {
+        return (
+          <ExpanseItem
+            key={expense.id}
+            title={expense.title}
+            date={expense.date}
+            amount={expense.amount}
+          />
+        );
+      })}
+    </Card>
+  );
+};
 
 export default Expense;
